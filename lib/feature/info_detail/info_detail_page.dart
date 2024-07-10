b8238ead71ab8673cd88bf48050945d28bfcf2da
Ngurah Deva <wirandanaputra@gmail.com>
2024-07-10 22:53:17 +0800

    fix: add scroll on info detail

--- lib/feature/info_detail/info_detail_page.dart
+++ lib/feature/info_detail/info_detail_page.dart
@@ -17,91 +17,93 @@ class InfoDetailPage extends StatelessWidget {
         body: GetBuilder<InfoDetailController>(
           builder: (controller) {
             print(controller.songData?.image);
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                const SizedBox(height: 20),
-                const BackButtonWidget(),
-                const SizedBox(height: 40),
-                Visibility(
-                  visible: controller.songData?.image != null && controller.songData?.image != "",
-                  child: Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 24),
-                    child: CachedNetworkImage(
-                      imageUrl: Environments.pocketBaseDevFiles(
-                        collectionId: controller.songData!.collectionId!,
-                        recordId: controller.songData!.id!,
-                        fileName: controller.songData!.image!,
-                      ),
-                      imageBuilder: (context, imageProvider) => Container(
-                        width: Get.width,
-                        height: 200,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          image: DecorationImage(
-                              image: imageProvider,
-                              fit: BoxFit.cover,
+            return SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  const SizedBox(height: 20),
+                  const BackButtonWidget(),
+                  const SizedBox(height: 40),
+                  Visibility(
+                    visible: controller.songData?.image != null && controller.songData?.image != "",
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 24),
+                      child: CachedNetworkImage(
+                        imageUrl: Environments.pocketBaseDevFiles(
+                          collectionId: controller.songData!.collectionId!,
+                          recordId: controller.songData!.id!,
+                          fileName: controller.songData!.image!,
+                        ),
+                        imageBuilder: (context, imageProvider) => Container(
+                          width: Get.width,
+                          height: 200,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            image: DecorationImage(
+                                image: imageProvider,
+                                fit: BoxFit.cover,
+                            ),
                           ),
                         ),
-                      ),
-                      placeholder: (context, url) => Container(
-                        width: Get.width,
-                        height: 200,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: AppColors.grey,
-                        ),
-                        child: const Center(
-                          child: Icon(
-                            Icons.image_outlined,
-                            size: 75,
+                        placeholder: (context, url) => Container(
+                          width: Get.width,
+                          height: 200,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
                             color: AppColors.grey,
                           ),
+                          child: const Center(
+                            child: Icon(
+                              Icons.image_outlined,
+                              size: 75,
+                              color: AppColors.grey,
+                            ),
+                          ),
                         ),
-                      ),
-                      errorWidget: (context, url, error) => Container(
-                        width: Get.width,
-                        height: 200,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: AppColors.grey,
-                        ),
-                        child: const Center(
-                          child: Icon(
-                            Icons.image_outlined,
-                            size: 75,
-                            color: AppColors.black,
+                        errorWidget: (context, url, error) => Container(
+                          width: Get.width,
+                          height: 200,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: AppColors.grey,
+                          ),
+                          child: const Center(
+                            child: Icon(
+                              Icons.image_outlined,
+                              size: 75,
+                              color: AppColors.black,
+                            ),
                           ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(height: 20),
-                Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 24),
-                  child: Text(
-                    controller.songData?.title ?? "-",
-                    style: Theme.of(context).textTheme.displayLarge!.copyWith(
-                      color: AppColors.black,
-                      fontWeight: FontWeight.w700
-                    ),
-                    textAlign: TextAlign.start,
-                  ),
-                ),
-                const SizedBox(height: 20),
-                Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 24),
-                  child: HtmlWidget(
-                    controller.songData?.history ?? "-",
-                    textStyle: Theme.of(context).textTheme.headlineSmall!.copyWith(
-                      color: AppColors.black,
-                      fontWeight: FontWeight.w400,
+                  const SizedBox(height: 20),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 24),
+                    child: Text(
+                      controller.songData?.title ?? "-",
+                      style: Theme.of(context).textTheme.displayLarge!.copyWith(
+                        color: AppColors.black,
+                        fontWeight: FontWeight.w700
+                      ),
+                      textAlign: TextAlign.start,
                     ),
                   ),
-                ),
-              ],
+                  const SizedBox(height: 20),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 24),
+                    child: HtmlWidget(
+                      controller.songData?.history ?? "-",
+                      textStyle: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                        color: AppColors.black,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             );
           },
         ),
